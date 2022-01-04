cdea3a474ffc2ef0c11865dfd430e56bacc84f58
nadsoft61 <you@example.com>
2022-01-04 21:02:51 +0530

    First

---
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,15 @@
+import "featuredInfo.css";
+
+export default function FeaturedInfo() {
+    return (
+        <div className="featured">
+            <div className="featuredItem">
+                <span className="featuredTitle">Revenue</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$2,415</span>
+                    <span className="featuredMoney">-11.4</span>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './topbar.css';
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topleft">
+                    <span className="logo">Zaid Admin</span>
+                </div>
+                <div className="topright">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src="" alt="" className="topAvatar" />
+                </div>
+            </div>
+        </div>
+    )
+}
