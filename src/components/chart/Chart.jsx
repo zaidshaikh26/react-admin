fbfae0a0e473710c61bc4ad5775f682d958083f0
nadsoft61 <you@example.com>
2022-01-04 21:57:32 +0530

    third

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,34 +1,18 @@
 import "./chart.css";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-export default function Chart() {
+export default function Chart({title, data, dataKey, grid}) {
 
-    const data = [
-        {
-            name: 'Jan',
-            "Active User": 4000,
-        },
-        {
-            name: 'Feb',
-            "Active User": 3000,
-        },
-        {
-            name: 'Mar',
-            "Active User": 2000,
-        },
-        {
-            name: 'Apr',
-            "Active User": 1000,
-        },
-      ];
-
+    
     return (
         <div className="chart">
-            <h3 className="chartTitle">Sales Analytics</h3>
+            <h3 className="chartTitle">{title}</h3>
             <ResponsiveContainer width="100%" aspect={4/1}>
                 <LineChart data={data}>
                     <XAxis dataKey="name" stroke="#5550bd" />
-                    <Line type="monotone" dataKey="Active User" stroke="#5550bd"/>
+                    <Line type="monotone" dataKey={dataKey} stroke="#5550bd"/>
+                    <Tooltip/>
+                    {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5"/>}
                 </LineChart>
             </ResponsiveContainer>
         </div>
---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active User": 4000,
+    },
+    {
+        name: 'Feb',
+        "Active User": 3000,
+    },
+    {
+        name: 'Mar',
+        "Active User": 2000,
+    },
+    {
+        name: 'Apr',
+        "Active User": 1000,
+    },
+    {
+        name: 'May',
+        "Active User": 1000,
+    },
+    {
+        name: 'June',
+        "Active User": 1000,
+    },
+    {
+        name: 'July',
+        "Active User": 1000,
+    },
+    {
+        name: 'August',
+        "Active User": 1000,
+    },
+    {
+        name: 'Sept',
+        "Active User": 1000,
+    },
+    {
+        name: 'Oct',
+        "Active User": 1000,
+    },
+    {
+        name: 'Nov',
+        "Active User": 1000,
+    },
+    {
+        name: 'Dec',
+        "Active User": 1000,
+    },
+  ];
